22d46235ea8e7dfbaf6de8dfadaf9b20869df402
prakash204 <shashiprakash1729@gmail.com>
2022-04-07 14:35:17 +0530

    dummy

--- mediapipe_test/demo_3.py
+++ mediapipe_test/demo_3.py
@@ -120,8 +120,6 @@ def main():
         pTime = cTime
 
 
-        #cv2.putText(img,str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
-        #flipped = cv2.flip(img, 1)
         cv2.imshow("Image",img)
         cv2.waitKey(1)
 
